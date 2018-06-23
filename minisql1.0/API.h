7c997e10f1ec463165a964a392df08b667af6343
sanyangji <a1001_jason@163.com>
2018-06-23 21:11:32 +0800

    Add files via upload

--- minisql1.0/API.h
+++ minisql1.0/API.h
@@ -3,8 +3,9 @@
 
 #include "Attribute.hpp"
 #include "Condition.hpp"
-//#include "Minisql.h"
-//#include "IndexInfo.h"
+//#include "RecordManager.h"
+//#include "CatalogManager.h"
+//#include "IndexManager.h"
 #include <string>
 #include <cstring>
 #include <vector>
@@ -13,9 +14,10 @@
 class API{
 
 public:
-	
+	RecordManager *record_manager;
+	CatalogManager *catalog_manager;
+	IndexManager *index_manager;
 	// attribute_vector存储表的所有attribute，location存储主键在vector中是第几个
-	//void API::create_table(string table_name, vector<Attribute> &attribute_vector, int primary_key_location)
 	void create_table(string table_name, vector<Attribute> & attribute_vector, int primary_key_location);
 	// 索引名 | 表名 | 列名
 	void create_index(string index_name, string table_name, string attribute_name);
@@ -27,13 +29,14 @@ public:
 	void record_insert(string table_name, vector<string> &value_vector);
 	// 表名 | condition_vector存储筛选条件，member有属性Attribute_Name、操作符operate、比较的值value
 	void record_delete(string table_name, vector<Condition> &condition_vector);
-
+	void update(string table_name, string attribute_name, string new_value, vector<Condition> &condition_vector);
 private:
 	void attribute_print(vector<Attribute> &attribute_vector);
 	bool check_table(string table_name);
 	bool check_index(string index_name);
 	void attribute_name_print(string table_name);
 	string index_file_name_get(string index_name);
+	int getType(int type);
 };
 
 #endif
